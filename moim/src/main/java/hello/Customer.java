a4f03685707491fd894b032a24eabbb834eda2ff
v0o0v <v0o0v@daum.net>
2016-04-05 00:08:47 +0900

    최초 공유

---
+++ moim/src/main/java/hello/Customer.java
@@ -0,0 +1,32 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	public Customer(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+}
