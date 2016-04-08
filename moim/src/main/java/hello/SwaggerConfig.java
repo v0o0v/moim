eb36461505e1e5675b6bbf8ef14c12360ff19e65
v0o0v <v0o0v@daum.net>
2016-04-08 13:10:34 +0900

    STS 설정 추가

--- moim/src/main/java/hello/SwaggerConfig.java
+++ moim/src/main/java/hello/SwaggerConfig.java
@@ -15,7 +15,9 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
 }
