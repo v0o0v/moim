39fbe93b468bab414920565bbe52a10bf32eb5e5
v0o0v <v0o0v@daum.net>
2016-04-09 19:43:31 +0900

    H2 DB Console 추가

--- moim/src/main/java/com/v0o0v/moim/Application.java
+++ moim/src/main/java/com/v0o0v/moim/Application.java
@@ -1,6 +1,5 @@
 package com.v0o0v.moim;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,34 +25,11 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// save a couple of customers
-		repository.save(new Member("홍길동", "홍길동입니다"));
-		repository.save(new Member("AAA", ""));
-		repository.save(new Member("zdfdsf adfsdf", "sdfsf"));
-		repository.save(new Member("1", "Palmer"));
-		repository.save(new Member("2", "Dessler"));
 
-		// fetch all customers
-		log.info("Customers found with findAll():");
-		log.info("-------------------------------");
-		for (Member customer : repository.findAll()) {
-			log.info(customer.toString());
+		for (int i = 1; i < 5; i++) {
+			repository.save(new Member("Member"+i, "Description_"+i));
 		}
-		log.info("");
 
-		// fetch an individual customer by ID
-		Member customer = repository.findOne(1L);
-		log.info("Customer found with findOne(1L):");
-		log.info("--------------------------------");
-		log.info(customer.toString());
-		log.info("");
-
-		// fetch customers by last name
-		log.info("Customer found with findByLastNameStartsWithIgnoreCase('홍길동'):");
-		log.info("--------------------------------------------");
-		for (Member bauer : repository.findByNameStartsWithIgnoreCase("홍길동")) {
-			log.info(bauer.toString());
-		}
-		log.info("");
 	}
 
 }
---
+++ moim/src/main/java/com/v0o0v/moim/config/H2Config.java
@@ -0,0 +1,17 @@
+package com.v0o0v.moim.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2Config {
+	@Bean
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/db/*");
+		return registrationBean;
+	}
+
+}
