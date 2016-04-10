700ed92dae606328a9dc01c514194ccf15ee5b4a
v0o0v <v0o0v@daum.net>
2016-04-10 15:55:04 +0900

    ...

--- moim/src/main/java/com/v0o0v/moim/repository/MemberRepository.java
+++ moim/src/main/java/com/v0o0v/moim/repository/MemberRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.v0o0v.moim.domain.Member;
 
-@RepositoryRestResource(path="members")
-//@RepositoryRestResource(collectionResourceRel="member", path="member")
+@RepositoryRestResource(collectionResourceRel = "member", path = "member")
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	List<Member> findByName(String name);
+
 	List<Member> findByNameStartsWithIgnoreCase(String name);
+
 	List<Member> findByNameAndDesc(String name, String desc);
 }
